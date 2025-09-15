9e6f596d3144c9c98fd1f1429640f290a9a7bbb5
Khalid Ag Mohamed Aly <123359678+KMohamed20@users.noreply.github.com>
2025-09-15 21:46:36 +0100

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import Header from './components/Header'
+import Overview from './pages/Overview'
+import Projects from './pages/Projects'
+import Profile from './pages/Profile'
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <div className="flex h-screen bg-gray-50">
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Main Content */}
+        <div className="flex-1 flex flex-col overflow-hidden">
+          <Header />
+          <main className="flex-1 overflow-y-auto p-6">
+            <Routes>
+              <Route path="/" element={<Overview />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </Router>
+  )
+}
+
+export default App
